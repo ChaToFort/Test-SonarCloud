292dd4aa92573c130b91d3d806b8cb172d9eb2bb
ChaToFort <gogniatpierre@gmail.com>
2022-02-11 16:44:10 +0100

    Ajoutez des fichiers projet.

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<?php
+$nb = 9;
+
+for($i=1; $i <= $nb; $i++) {
+
+	echo $i ;
+	for ($j=1; $j <= $nb; $j++){
+		}
+	echo $j * $i;
+	echo '</p>';
+	}
+
+
+
+///		if ($j - $i == 0){
+///
+	///			echo $i * $j . '- ';
+	///		}elseif($i == $j){
+	///			echo $i . ' ';
+		///	}elseif($i == 1){
+	///			echo $i * $j . 't ';
+	///		}else{
+	///			echo $i * $j . ' ';
+	///		} 
+?>
+</html>
